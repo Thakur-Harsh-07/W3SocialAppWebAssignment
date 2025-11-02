83a16ff8a3cbd24db314eec1696e80f30c4fa886
Thakur-Harsh-07 <harshchauhan7081@gmail.com>
2025-11-02 17:14:00 +0530

    Fixed URL

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = 'http://https://socialwebassignment.onrender.com/api/v1';
+const API_BASE_URL = 'https://socialwebassignment.onrender.com/api/v1';
 
 const api = axios.create({
   baseURL: API_BASE_URL,
