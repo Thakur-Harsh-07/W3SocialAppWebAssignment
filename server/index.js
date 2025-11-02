93c9e65ca3559aa59b8eba55ffc68abab9166231
Thakur-Harsh-07 <harshchauhan7081@gmail.com>
2025-11-02 17:05:42 +0530

    Deployed

--- server/index.js
+++ server/index.js
@@ -14,7 +14,7 @@ connectDB();
 // CORS configuration 
 app.use(
 	cors({
-		origin: "http://localhost:5173",
+		origin: "*",
 		credentials: true,
 		methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
 		allowedHeaders: ["Content-Type", "Authorization"],
